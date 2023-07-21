94ead4b0d9a47005454caaaf03fc3aeba2f49686
Raf Gemmail <rafiq@dreamthought.com>
2023-07-21 23:32:07 +1200

    Moved derivation to a flake

--- flake.nix
+++ flake.nix
@@ -1,19 +1,33 @@
 {
-  description = "Pyenv (Impure) Development Environment";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.05";
-    pyenv.url = "path:./";
-  };
+  description = "A flake for a local impure pyenv";
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
 
-  outputs = { self, nixpkgs }:
-    let 
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system}.pkgs;
-      lib = nixpkgs.lib;
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "Pyenv shell";
-        buildInputs =  [ pyenv ];  
+  outputs = {self, nixpkgs} @inputs: let
+    pyenvRepo = builtins.fetchGit{
+      "url" =  "https://github.com/pyenv/pyenv";
+      "rev" = "b81204c08bf8ef8ab2ea0daeb721ee08020a26a0";
+#      "sha256" = "12h96h2fz805r02z4nal4hbasjf8cbbdq7bawbxcvl5pf1g9rwhk";
+    };
+
+  in {
+
+    defaultPackage.x86_64-linux = 
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        pname = "pyenv";
+        version = "2.3.22";
+        src = pyenvRepo;    
+        nativeBuildInputs = [pkgs.bats pkgs.git];
+        dontConfigure = true;
+        dontBuild = true;
+        installPhase = ''
+           mkdir -p $out
+           # Copy clone to $out
+           cp -r ${pyenvRepo.outPath}/* $out
+           
+           # Remind the user to set PYENV_ROOT
+           echo To be impure, please Run 'mkdir ~/.pyenv-nix' and add 'export PYENV_ROOT=$HOME/.pyenv-nix' to your profile
+        '';
       };
   };
 }
