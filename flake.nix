e7228e748a0487e5c99cc2bc9101c8c52a0e7e47
Raf Gemmail <rafiq@dreamthought.com>
2023-07-23 18:15:02 +1200

    Fixed include paths

--- flake.nix
+++ flake.nix
@@ -17,14 +17,24 @@
       devShells.${system}.default = pkgs.mkShell rec  {
         name = "pyenv Cli Environment";
         nativeBuildInputs = with pkgs; [
-          pkg-config
+          pkg-config zlib.dev zlib.dev 
+          bzip2.dev expat.dev 
+          xz.dev sqlite.dev readline.dev 
+          openssl.dev
         ];
         buildInputs = with pkgs; [
-          bzip2 expat xz libffi libxcrypt gdbm sqlite readline ncurses openssl
           pyenv
+          zlib
+          bzip2 expat xz libffi libxcrypt gdbm sqlite readline ncurses openssl
+ 
         ];
         shellHook = ''
           export LD_LIBRARY_PATH=${pkgs.zlib}/lib;
+          export LDFLAGS="-L${pkgs.zlib}/lib $LDFLAGS";
+          export CPPFLAGS="-I${pkgs.zlib.dev}/include $CPPFLAGS";
+          export CFLAGS="-I${pkgs.zlib.dev}/include $CFLAGS";
+          export CFLAGS="-I${pkgs.xz.dev}/include $CFLAGS";
+          export CFLAGS="-I${pkgs.sqlite.dev}/include $CFLAGS";
           mkdir -p ~/.pyenv
           export PYENV_ROOT=$HOME/.pyenv
         '';
