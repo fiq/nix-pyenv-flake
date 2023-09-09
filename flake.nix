5e319029a464cbd6f3a0b1e9c58630f2affbd245
Raf Gemmail <rafiq@dreamthought.com>
2023-09-10 00:18:22 +1200

    pkgs - Added cuda and nvidia libraries

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
         buildInputs = with pkgs; [
           pyenv
           zlib tk
-          bzip2 expat xz libffi libxcrypt gdbm sqlite readline ncurses openssl libffi
+          bzip2 expat xz libffi libxcrypt gdbm sqlite readline ncurses openssl libffi cudatoolkit linuxPackages.nvidia_x11
         ];
         shellHook = ''
           export PKG_CONFIG_BIN=${pkgs.pkg-config}/bin/pkg-config;
@@ -34,6 +34,8 @@
           export LD_LIBRARY_PATH="$($PKG_CONFIG_BIN --variable=libdir tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline|sed -e's/\s/:/g') $LDFLAGS";
           export LD_LIRBARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib":$LD_LIBRARY_PATH;
           export LD_LIBRARY_PATH="${pkgs.gcc-unwrapped.lib}/lib":$LD_LIRBARY_PATH;
+          export LD_LIBRARY_PATH="${pkgs.cudatoolkit.lib}/lib":$LD_LIRBARY_PATH;
+          export LD_LIBRARY_PATH="${pkgs.linuxPackages.nvidia_x11}/lib":$LD_LIRBARY_PATH;
           export LDFLAGS="$($PKG_CONFIG_BIN --libs tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline) $LDFLAGS";
           export CPPFLAGS="$($PKG_CONFIG_BIN --cflags tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline) $CPPFLAGS";
           export CFLAGS="$($PKG_CONFIG_BIN --cflags tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline) $CFLAGS";
