78f79a491945c037af53f6b1933f83f42be79de9
Raf Gemmail <rafiq@dreamthought.com>
2024-01-03 11:10:40 +1300

    fix - pkgconfig uses openblas64 and bump of cuda to 12

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "A flake for a local impure pyenv";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
 
   outputs = {self, nixpkgs} @inputs: let
     system = "x86_64-linux";
@@ -9,6 +9,7 @@
       inherit system;
       overlays = [ (import ./pyenv-overlay.nix) ];
       config.allowUnfree = true;
+      config.cudaSupport = true;
     };
 
  
@@ -21,25 +22,25 @@
           bzip2.dev expat.dev 
           xz.dev sqlite.dev readline.dev 
           openssl.dev tk
-          curl portaudio
+          curl portaudio openblas
         ];
         buildInputs = with pkgs; [
           pyenv
           zlib tk
           bzip2 expat xz libffi libxcrypt gdbm sqlite readline ncurses openssl libffi
-          cudatoolkit linuxPackages.nvidia_x11 cudaPackages.cuda_cudart openblas portaudio
+          cudaPackages_12_2.cudatoolkit linuxPackages.nvidia_x11 cudaPackages_12_2.cuda_cudart openblas portaudio
         ];
         shellHook = ''
           export PKG_CONFIG_BIN=${pkgs.pkg-config}/bin/pkg-config;
 
-          export LD_LIBRARY_PATH="$($PKG_CONFIG_BIN --variable=libdir tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline openblas portaudio-2.0|sed -e's/\s/:/g') $LDFLAGS";
+          export LD_LIBRARY_PATH="$($PKG_CONFIG_BIN --variable=libdir tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline openblas64 portaudio-2.0|sed -e's/\s/:/g') $LDFLAGS";
           export LD_LIRBARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib":$LD_LIBRARY_PATH;
           export LD_LIBRARY_PATH="${pkgs.gcc-unwrapped.lib}/lib":$LD_LIRBARY_PATH;
           export LD_LIBRARY_PATH="${pkgs.cudatoolkit.lib}/lib":$LD_LIRBARY_PATH;
           export LD_LIBRARY_PATH="${pkgs.linuxPackages.nvidia_x11}/lib":$LD_LIRBARY_PATH;
-          export LDFLAGS="$($PKG_CONFIG_BIN --libs tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline portaudio-2.0) $LDFLAGS";
-          export CPPFLAGS="$($PKG_CONFIG_BIN --cflags tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline portaudio-2.0) $CPPFLAGS";
-          export CFLAGS="$($PKG_CONFIG_BIN --cflags tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline portaudio-2.0) $CFLAGS";
+          export LDFLAGS="$($PKG_CONFIG_BIN --libs tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline openblas64 portaudio-2.0) $LDFLAGS";
+          export CPPFLAGS="$($PKG_CONFIG_BIN --cflags tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline openblas64 portaudio-2.0) $CPPFLAGS";
+          export CFLAGS="$($PKG_CONFIG_BIN --cflags tk libffi openssl zlib bzip2 sqlite3 liblzma ncurses readline openblas64 portaudio-2.0) $CFLAGS";
 
           if [[ -z "$PYENV_ROOT" ]]; then
             PYENV_ROOT=$HOME/.pyenv
